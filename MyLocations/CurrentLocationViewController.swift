c3a5f107713bdb08c65b52be548c38c2719ffa12
vancouver29 <vancouver2990@gmail.com>
2020-03-31 19:36:44 +0200

    updated CurrentLocationViewController.swift

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class CurrentLocationViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longtitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UILabel!
+    @IBOutlet weak var getButtion: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    //MARK:- Actions
+    @IBAction func getLocation() {
+        
+    }
 }
 
