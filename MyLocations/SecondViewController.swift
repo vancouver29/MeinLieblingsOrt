b376f64b1713acfa6003195b1c164be9323fd2fb
vancouver29 <vancouver2990@gmail.com>
2020-03-31 13:37:07 +0200

    created project MeinLieblingsOrt

---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Khanh Bui on 31.03.20.
+//  Copyright © 2020 Khanh Bui. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
