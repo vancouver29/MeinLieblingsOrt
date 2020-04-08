56db0c3d8158561f92cde854f31e05e49c9ad116
vancouver29 <vancouver2990@gmail.com>
2020-04-08 17:15:44 +0200

    date formatting with Dateformatter closure

--- MyLocations/LocationDetailsViewcontroller.swift
+++ MyLocations/LocationDetailsViewcontroller.swift
@@ -9,16 +9,42 @@
 import UIKit
 import CoreLocation
 
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    print("This is dateFormatter")
+    return formatter
+}()
+
 class LocationDetailsViewController: UITableViewController {
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
-    @IBOutlet weak var longtitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
     var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
     var placemark: CLPlacemark?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        
+        if let placemark = placemark {
+            addressLabel.text = string(from: placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+        
+        dateLabel.text = format(date: Date())
+    }
     
     //MARK:- Actions
     @IBAction func done() {
@@ -27,5 +53,34 @@ class LocationDetailsViewController: UITableViewController {
     
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
+    }
+    
+    //MARK:- Helpers Methods
+    func format(date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
+    
+    func string(from placemark: CLPlacemark) -> String {
+        var text = ""
+        
+        if let s = placemark.subThoroughfare {
+            text += s + " "
+        }
+        if let s = placemark.thoroughfare {
+            text += s + ", "
+        }
+        if let s = placemark.locality {
+            text += s + ", "
+        }
+        if let s = placemark.administrativeArea {
+            text += s + " "
+        }
+        if let s = placemark.postalCode {
+            text += s + ", "
+        }
+        if let s = placemark.country {
+            text += s
+        }
+        return text 
     }
 }
